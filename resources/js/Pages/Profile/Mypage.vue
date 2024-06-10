088e87dc682ac9225d88353faf84b1c91ced77c1
taishi <wilas.tk09@gmail.com>
2024-06-10 18:04:12 +0900

    mypageのレイアウト修正

--- resources/js/Pages/Profile/Mypage.vue
+++ resources/js/Pages/Profile/Mypage.vue
@@ -30,7 +30,7 @@ const props = defineProps({
         </template>
 
         <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8 min-h-screen">
                 <div v-if="$page.props.jetstream.canUpdateProfileInformation">
 
                     <ShowProfile 
--- resources/js/Pages/Profile/ShowProfile.vue
+++ resources/js/Pages/Profile/ShowProfile.vue
@@ -13,6 +13,9 @@ import Textarea from '@/Components/Textarea.vue';
 import SelectBox from '@/Components/SelectBox.vue';
 import SectionBorder from '@/Components/SectionBorder.vue';
 
+// baseurl
+const baseUrl = import.meta.env.VITE_APP_BASE_URL;
+
 // 親コンポーネント(Create.vue)からオブジェクト、配列の受け渡し
 const props = defineProps({
     user: Object,
@@ -24,6 +27,10 @@ const props = defineProps({
 console.log(props);
 
 const {user, reviews, favorites} = usePage().props;
+
+const formattedDate = computed(() => {
+  return props.user.created_at.split('T')[0];
+});
 
 // setup ブロック内で genderOptions を定義
 const genderOptions = ref([
@@ -121,35 +128,37 @@ const clearPhotoFileInput = () => {
         photoInput.value.value = null;
     }
 };
-
-console.log(process.env.NODE_ENV);
+console.log(reviews[0].sauna.id);
 </script>
 
 <template>
-  <div class="grid grid-cols-12 gap-4 bg-white p-12 mb-8 rounded-lg shadow">
+  <div class="grid grid-cols-12 gap-4 bg-blue-950 p-12 mb-8 rounded-lg shadow text-white">
     <div class="col-span-12">
-      <p>登録日:{{ user.created_at }}</p>
+      <p class="text-xl font-bold text-center">プロフィール</p>
     </div>
-    <div class="col-span-2">
+    <div class="col-span-12">
+      <p class="text-xs text-right">登録日:{{ formattedDate }}</p>
+    </div>
+    <div class="col-span-12">
       <span
         class="block rounded-full w-20 h-20 bg-cover bg-no-repeat bg-center"
         :style="'background-image: url(\'' + user.profile_photo_url + '\');'"
       />
     </div>
-    <div class="col-span-4">
-      {{ user.name }}
+    <div class="col-span-12">
+      ユーザ名：{{ user.name }}
     </div>
     <div class="col-span-12 grid grid-cols-12 gap-4">
-      <div class="col-span-2">
+      <div class="col-span-5 md:col-span-3">
         ホームサウナ
       </div>
-      <div class="col-span-10">
+      <div class="col-span-7 mad:col-span-9">
         {{ user.home_sauna }}
       </div>
-      <div class="col-span-2">
-        プロフィール
+      <div class="col-span-12 sm:col-span-5 md:col-span-3">
+        自己紹介
       </div>
-      <div class="col-span-10">
+      <div class="col-span-12 sm:col-span-7 mad:col-span-9">
         {{ user.profile_text }}
       </div>
     </div>
@@ -167,13 +176,13 @@ console.log(process.env.NODE_ENV);
   </div>
 
   <div class="grid grid-cols-6 gap-6">
-    <div class="col-span-2 bg-white p-12 rounded-lg shadow">
-      検索
+    <div class="col-span-6 md:col-span-2 bg-gray-100 p-12 rounded-lg shadow">
+      検索(準備中)
     </div>
     
-    <div class="col-span-4 bg-white p-12">
+    <div class="col-span-6 md:col-span-4 bg-white p-12">
       <div class="mb-2">
-        サ活一覧(usersに紐づいたreviews)
+        サ活一覧
       </div>
 
       <div  v-for="review in reviews"
@@ -189,14 +198,14 @@ console.log(process.env.NODE_ENV);
           ユーザー名: {{ user.name }}<br>
           訪問日:{{ review.visited_date }}
         </div>
-        <div class="col-span-6">
-          {{ review.sauna.facility_name }}  [{{ review.sauna.prefecture.name }}]
+        <div class="col-span-6 cursor-pointer hover:text-blue-700">
+          <a :href="`${baseUrl}saunas/${ review.sauna.id }`" class="block">{{ review.sauna.facility_name }}  [{{ review.sauna.prefecture.name }}]</a>
         </div>
         <div class="col-span-6">
-          タイトル {{ review.title }}
+          タイトル: {{ review.title }}
         </div>
         <div class="col-span-6">
-          文章 {{ review.content }}
+          内容: {{ review.content }}
         </div>
       </div>
 
