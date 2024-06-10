76d4a3012624d088228ffb9efadaefbd16b1a0cf
taishi <wilas.tk09@gmail.com>
2024-06-10 10:03:09 +0900

    未ログイン時のprofile_photo_urlがnullで取得してしまいエラーとなっているのを修正

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -177,7 +177,7 @@ const logout = () => {
                             <div class="ml-3 relative">
                                 <Dropdown align="right" width="48">
                                     <template #trigger>
-                                        <button v-if="$page.props.jetstream.managesProfilePhotos" 
+                                        <button v-if="$page.props.jetstream.managesProfilePhotos && $page.props.auth.user" 
                                                 class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
                                             <img class="h-8 w-8 rounded-full object-cover" 
                                                  :src="$page.props.auth.user.profile_photo_url" 
